baa3fe90d45876ec395eed863d17019683926c7d
Pau <pau.soler.valades@gmail.com>
2020-09-22 00:57:08 +0200

    No va literalment res. Vull dir, res fa el que ha de fer, toca preguntar xd

--- practica1/dibujar-puntos.c
+++ practica1/dibujar-puntos.c
@@ -12,6 +12,7 @@
 
 #include <GL/glut.h>
 #include <stdio.h>
+#include <math.h>
 #include "hiruki.h"
 
 /*
@@ -86,34 +87,38 @@ void determinar_orden(hiruki triangulo, punto *Aptr, punto* Bptr, punto* Cptr)
 {
 	
 	punto aux;
-	*Aptr = triangulo.p1;
-	*Bptr = triangulo.p2;
-	*Cptr = triangulo.p3;
 	
 	if(triangulo.p1.y > triangulo.p3.y)
 	{
 		printf("Canvio A per C\n");
-		*Aptr = triangulo.p3;
-		*Cptr = triangulo.p1;
+		aux = triangulo.p1;
+		triangulo.p1 = triangulo.p3;
+		triangulo.p3 = triangulo.p1;
 	}
 	
 	if(triangulo.p1.y > triangulo.p2.y)
 	{
 		printf("Canvio A per B\n");
-		*Aptr = triangulo.p2;
-		*Bptr = triangulo.p1;
+		aux = triangulo.p1;
+		triangulo.p1 = triangulo.p2;
+		triangulo.p2 = aux;
 	}
 	
 	if(triangulo.p2.y > triangulo.p3.y)
 	{
 		printf("Canvio B per C\n");
-		*Aptr = triangulo.p2;
-		*Bptr = triangulo.p1;
+		aux = triangulo.p2;
+		triangulo.p2 = triangulo.p3;
+		triangulo.p3 = triangulo.p2;
 	}
+	
+	*Aptr = triangulo.p1;
+	*Bptr = triangulo.p2;
+	*Cptr = triangulo.p3;
 }
 
 /*punto o directamente void?*/
-void calcular_interseccion(punto A, punto B, float *pin, int h)
+void calcular_interseccion(punto A, punto B, int *pin, int h)
 {
 	int x, y;
 	float y_bis, prova;
@@ -123,8 +128,29 @@ void calcular_interseccion(punto A, punto B, float *pin, int h)
 	
 	y_bis = h - A.y;
 	
-	*pin = A.x+(x*y_bis/y);
+	*pin = (int) round(A.x+(x*y_bis/y));
 	
+	//printf("%d\n", *pin);
+	
+}
+
+void dibujar_pixel(int x, int y)
+{
+
+	float u,v;
+	unsigned char* colorv;
+	unsigned char r,g,b;
+	
+	u = x/500.0;
+	v = y/500.0;
+	colorv = color_textura(u, v); //TODO: si esta función es correcta se ve la foto en la ventana
+	r= colorv[0];
+	g=colorv[1];
+	b=colorv[2];     
+	glBegin( GL_POINTS );
+	glColor3ub(r,g,b);
+	glVertex3f(x,y,0.);
+	glEnd();
 }
 
 
@@ -140,17 +166,22 @@ void dibujar_triangulo(hiruki triangulo)
 	
 	int h,x;
 	punto *Aptr, *Bptr, *Cptr;
-	float pin1, pin2;
+	int pin_left, pin_right;
 	
-	//aquests valors s'hauran de carregar de determinar_orden.
+	h = 0;
+	x = 0;
+	
+	//aquests valors s'hauran de carregar de determinar_orden, assumim que venen ordenats tal que A es el mes alt, C el mes baix i B l'altre
 	
 		
 	Aptr = &triangulo.p1;
 	Bptr = &triangulo.p2;
 	Cptr = &triangulo.p3;
 	
+	//triangulo es una copia, puedo hacer con el lo que quiera
+	//determinar_orden(triangulo, Aptr, Bptr, Cptr);
 	
-	//determinar_orden(triangulo, &Aptr, &Bptr, &Cptr);
+	//printf("%f, %f, %f\n", Aptr->y, Bptr->y, Cptr->y);
 	
 	/*
 	print_triangle(triangulo);
@@ -159,16 +190,20 @@ void dibujar_triangulo(hiruki triangulo)
 	determinar_orden(triangulo, &triangulo.p1, &triangulo.p2, &triangulo.p3);
 	*/
 
-	/*
-	for(h=Aptr->y; h>Bptr->y; h--)
+	//començes a la altura d'A i vas cap a B pujant la altura
+	for(h=Aptr->y; h<=Bptr->y; h++)
 	{
-		calcular_interseccion(*Aptr, *Bptr, &pin1, h);
+		calcular_interseccion(*Aptr, *Cptr, &pin_left, h);
+		calcular_interseccion(*Aptr, *Bptr, &pin_right, h);
+		printf("%d, %d\n", pin_left, pin_right);
+		for(x=pin_left; x<=pin_right; x++)
+		{
+			dibujar_pixel(x, h);
+		}
 		
 	}
-	*/
-	calcular_interseccion(*Aptr, *Bptr, &pin1, 150);
-	calcular_interseccion(*Cptr, *Bptr, &pin2, 150);
-	printf("%f, %f\n", pin1, pin2);
+	
+	printf("%d, %d\n", pin_left, pin_right);
 		
 	
 }
@@ -206,23 +241,19 @@ static void marraztu(void)
 	glLoadIdentity();
 	glOrtho(0.0, 500.0, 0.0, 500.0,-250.0, 250.0);
 
-	// por ahora dibujamos todos los pixels de la ventana de 500x500 con el color que devuelve la función color_textura
 	// pero hay que llamar a la función que dibuja un triangulo con la textura mapeada:
-	/* //TODO: amb les coordenanades del triangle treus les equacions de les rectes al pla i ho poses com a condició del for, molt semblant a com està escrit ja.
-	dibujar_triangulo(triangulosptr[indice]); //aquí el podem cridar com una llista, ja que el punter d'una taula és el mateix que el punter d'una llista, excepte que aquest el podem iterar.
-
-
-	*/
 	
 	dibujar_triangulo(triangulosptr[indice]);
 
-	/* Aquest bucle dibuixa tots els píxels de la pantalla, no res més. Això s'ha de fer a dibujar_triangulo adequadament.*/
+	glFlush();
+
+	/* Aquest bucle dibuixa tots els píxels de la pantalla, no res més. Això s'ha de fer a dibujar_triangulo adequadament.
 	for (i=0;i<500;i++)
 	    for (j=0;j<500;j++)
 		{
 			u = i/500.0;
 			v = j/500.0;
-			colorv=  color_textura(u, v); //TODO: si esta función es correcta se ve la foto en la ventana
+			colorv = color_textura(u, v); //TODO: si esta función es correcta se ve la foto en la ventana
 			r= colorv[0];
 			g=colorv[1];
 			b=colorv[2];     
@@ -232,6 +263,7 @@ static void marraztu(void)
 			glEnd();
 		}
 	glFlush();
+	*/
 }
 // This function will be called whenever the user pushes one key
 static void teklatua (unsigned char key, int x, int y)
